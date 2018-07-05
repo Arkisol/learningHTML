71e6c65e48734f66ba48d0ea1c71df41ba642b41
Greg Berezhnov <blackbeltdog20@gmail.com>
2018-07-05 14:16:23 -0400

    Everything

---
+++ Script1.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+var showMessage = function () {
+    document.getElementById("message").style.visibility = "visible";
+};
+var hideMessage = function () {
+    document.getElementById("message").style.visibility = "hidden";
+};
