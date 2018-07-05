c7823d0b8da772e4336b39db4981458e0fbaa3d1
Atif Mahmud <atifmahmud101@gmail.com>
2018-07-05 14:35:17 -0400

    changes

--- Script1.js
+++ Script1.js
@@ -1,7 +1,10 @@
 // JavaScript source code
 var showMessage = function () {
     document.getElementById("message").style.visibility = "visible";
+    document.getElementById("hideButton").style.visibility = "visible";
 };
+
 var hideMessage = function () {
     document.getElementById("message").style.visibility = "hidden";
+    document.getElementById("hideButton").style.visibility = "hidden";
 };
